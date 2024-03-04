ac4131327466697532746bf63469a680ecd47ada
andreza naara <naarafreire06@gmail.com>
2024-03-04 13:20:03 -0300

    aprendendo control-flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,12 @@ import { TemplateVariablesComponent } from './components/template/template-varia
     NewComponent,
     TemplateBindingComponent,
     TemplateVariablesComponent,
+    TemplateControlFlowComponent,
   ],
   styleUrls: ['./components/new-component/new-component.component.scss'],
   template: `
     <h1>Curso de Angular</h1>
-    <app-template-variables />
+    <app-template-control-flow />
   `,
 })
 export class AppComponent {
